775999e2f83373eac7b03214cedd738d94867888
Avadhut Pawar <itspawaravadhut2007@gmail.com>
2026-02-25 15:39:35 +0530

    updates

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+  registerUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser
+} = require("../controllers/userController");
+
+router.post("/register", registerUser);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+module.exports = router;
